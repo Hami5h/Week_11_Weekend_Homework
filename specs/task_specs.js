43317f2ddb5fa019f45ae44c9793a649269c37b3
Hamish <hamishfpso@gmail.com>
2017-12-08 10:32:56 +0000

    Task constructor created

---
+++ specs/task_specs.js
@@ -0,0 +1,2 @@
+const Task = require('../task.js')
+const assert = require('assert');
---
+++ task.js
@@ -0,0 +1,10 @@
+const Task = function() {
+  this.level = 0;
+  this.urgency = 0;
+  this.reward = 0;
+  this.tasks = [];
+  this.completed = false;
+}
+
+
+module.exports = Task
